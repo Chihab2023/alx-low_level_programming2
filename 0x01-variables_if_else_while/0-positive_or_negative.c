c3d55f1105a5b15e61d081043a55d949e51a5d01
chihab <chihab.sabri@gmail.com>
2023-06-16 18:08:43 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,13 +5,12 @@
 /**
  * main - determination of number if possitif of negatif or zero
  * Description: This program will assign a random number
- * to the variable n each time it is executed.
+ * to the variable n each time it is executed
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-
+int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
@@ -20,6 +19,5 @@ int main(void)
 		printf("%d is positive\n", n);
 	else
 		printf("%d is negative\n", n);
-	
 	return (0);
 }
