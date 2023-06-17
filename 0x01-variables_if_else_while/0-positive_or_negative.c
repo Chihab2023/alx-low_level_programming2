0364682b5e9ea58b0bfc055c0cd7f0a29f5b6cbf
chihab <chihab.sabri@gmail.com>
2023-06-16 17:56:37 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include <time.h>
 
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - determination of number if possitif of negatif or zero
+ * Description: This program will assign a random number
+ * to the variable n each time it is executed.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
