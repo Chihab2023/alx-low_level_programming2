8dd3677b7faf7a725fc9accf6079204f68b054ca
chihab <chihab.sabri@gmail.com>
2023-06-16 19:57:58 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print single digit
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%i", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
