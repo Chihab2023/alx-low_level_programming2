2242d1cbb3d2b604f9667602d6947aba51ae4bbb
chihab <chihab.sabri@gmail.com>
2023-06-19 18:35:10 -0700

    commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-        int x = 0;
+	int x = 0;
 
-        while (x <= 9)
-        {
-                putchar(x + '0');
-                x++;
-        }
-        putchar('\n);
-        return (0);
+	while (x <= 9)
+	{
+		putchar (x + '0');
+		x++;
+	}
+	putchar ('\n);
+	return (0);
 }
