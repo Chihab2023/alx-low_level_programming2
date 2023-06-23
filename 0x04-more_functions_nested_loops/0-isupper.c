a18ffc4e95c9d59869782fd74b2735e7a51907fc
chihab <chihab.sabri@gmail.com>
2023-06-22 19:07:41 -0700

    rr

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,17 +7,5 @@
  */
 int _isupper(int c)
 {
-	char uppercase = 'A';
-	int isupper;
-
-	for (isupper = 0; uppercase <= 'Z'; uppercase++)
-	{
-		if (c == uppercase)
-		{
-			isupper = 1;
-			break;
-		}
-	}
-
-	return (isupper);
+	if(c >=
 }
